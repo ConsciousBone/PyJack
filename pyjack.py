4a9cd32a288fd6985b4a27b3ff88a3cd71c13699
Evan <68429595+ConsciousBone@users.noreply.github.com>
2026-09-22 23:15:01 +0100

    Add menu error header, stop recursively calling main menu

--- pyjack.py
+++ pyjack.py
@@ -13,6 +13,9 @@ import os
 # Consts
 PYJACK_VERSION = 0
 
+# General variables
+menu_error_header = ""
+
 # General subroutines
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear') # clear terminal
@@ -26,33 +29,47 @@ def print_ascii_logo(page_name):
 ░░░░░░░░░░░░░░░░░░░░░░░░░
 PyJack (v{PYJACK_VERSION})
 {page_name}
+
+─────────────────────────
 ''')
 
 def menu_header(page_name):
-    clear()
+    global menu_error_header # let this subroutine use that var
+
+    clear() # very clean very fancy very wow
     print_ascii_logo(page_name)
+
+    if menu_error_header != "": # theres an error which needs showing, do it!
+        print(f"{menu_error_header}\n")
+        menu_error_header = "" # clear it to ensure it doesn't keep showing
 
 
 def main_menu():
-    menu_header("Main menu")
-    print('''\
+    while True:
+        global menu_error_header # let this subroutine use that var
+        menu_header("Main menu")
+        print('''\
 1) Play Blackjack
 2) Stats
 3) Config
 4) Quit
-    ''')
+        ''')
 
-    selected_menu_option = input("Select an option.\n> ") # get input
-    if selected_menu_option == "1":
-        print("play")
-    elif selected_menu_option == "2":
-        print("stats")
-    elif selected_menu_option == "3":
-        print("config")
-    elif selected_menu_option == "4":
-        print("quit")
-    else:
-        print("invalid")
+        selected_menu_option = input("Select an option.\n> ") # get input
+        if selected_menu_option == "1":
+            print("play")
+            break
+        elif selected_menu_option == "2":
+            print("stats")
+            break
+        elif selected_menu_option == "3":
+            print("config")
+            break
+        elif selected_menu_option == "4":
+            print("quit")
+            break
+        else:
+            menu_error_header = "Invalid option."
 
 
 # Run the damn thing
