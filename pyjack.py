ea0dc641ff83322336b78d5542f7c393ae179697
Evan <68429595+ConsciousBone@users.noreply.github.com>
2026-09-22 22:51:31 +0100

    Add menu options

--- pyjack.py
+++ pyjack.py
@@ -1,6 +1,8 @@
+# ░░░░░░░░░░░░░░░░░░░░░░░░░
 # ░█▀█░█░█░▀▀█░█▀█░█▀▀░█░█░
 # ░█▀▀░░█░░░░█░█▀█░█░░░█▀▄░
 # ░▀░░░░▀░░▀▀░░▀░▀░▀▀▀░▀░▀░
+# ░░░░░░░░░░░░░░░░░░░░░░░░░
 # PyJack - Blackjack but in a single Python file!
 # Created by @consciousbone for Hack Club Stardance
 # ASCII art from https://www.asciiart.eu/text-to-ascii-art
@@ -13,7 +15,7 @@ PYJACK_VERSION = 0
 
 # General subroutines
 def clear():
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system('cls' if os.name == 'nt' else 'clear') # clear terminal
 
 def print_ascii_logo(page_name):
     print(f'''
@@ -27,12 +29,30 @@ PyJack (v{PYJACK_VERSION})
 ''')
 
 def menu_header(page_name):
-    os.system('cls' if os.name == 'nt' else 'clear')
+    clear()
     print_ascii_logo(page_name)
 
 
 def main_menu():
     menu_header("Main menu")
+    print('''\
+1) Play Blackjack
+2) Stats
+3) Config
+4) Quit
+    ''')
+
+    selected_menu_option = input("Select an option.\n> ") # get input
+    if selected_menu_option == "1":
+        print("play")
+    elif selected_menu_option == "2":
+        print("stats")
+    elif selected_menu_option == "3":
+        print("config")
+    elif selected_menu_option == "4":
+        print("quit")
+    else:
+        print("invalid")
 
 
 # Run the damn thing
